cdfa3dcfaa941471d9316647ff45d27353f0ebd3
Jennifer Wilcox <jennifer.wilcox@adtran.com>
2025-01-02 11:26:40 -0600

    Add synthesis process
    I wanted to verify that basic_sdram is actually synthesizable. And it
    is. Since there is nothing driving the AXI channel, nextpnr optimizes
    everything away. But now we will be getting continuous verification that
    the RTL side of the code is always synthesizable.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: all clean simulations
 
-all: simulations
+all: bitstreams simulations
 
 define DEFINE_SIMULATION
 simulations: build/work/$(1)/meta-sim-run
@@ -20,9 +20,23 @@ build/work/$(1):
 	mkdir -p $$@
 endef
 
+define DEFINE_BITSTREAM
+bitstreams: build/work/$(1)/$(1).config
+
+build/work/$(1)/$(1).json: $(2) | build/work/$(1)
+	yosys -m ghdl -p "ghdl --no-formal $(2) -e toplevel ; synth_ecp5 -json $$@ -top toplevel"
+
+build/work/$(1)/$(1).config: build/work/$(1)/$(1).json
+	nextpnr-ecp5 --json $$< --textcfg $$@ --lpf synth/ulx3s/ulx3s_v20.lpf --85k --package CABGA381
+
+build/work/$(1):
+	mkdir -p $$@
+endef
+
 $(eval $(call DEFINE_SIMULATION,tb_spi_rx,src/spi_rx.vhd test/tb_spi_rx.vhd))
 $(eval $(call DEFINE_SIMULATION,tb_sdram,src/util.vhd test/test_util.vhd test/sim_sdram.vhd src/basic_sdram.vhd test/tb_sdram.vhd))
 $(eval $(call DEFINE_SIMULATION,tb_util,src/util.vhd test/tb_util.vhd))
+$(eval $(call DEFINE_BITSTREAM,ulx3s_sdram_test,src/util.vhd src/basic_sdram.vhd synth/ulx3s/sdram_test/toplevel.vhd))
 
 clean:
 	rm -rf build
---
+++ synth/ulx3s/sdram_test/toplevel.vhd
@@ -0,0 +1,60 @@
+library ieee;
+   use ieee.std_logic_1164.all;
+   use work.util.all;
+
+entity toplevel is
+    port(
+        clk_25mhz  : in     std_logic;
+        sdram_clk  : out    std_logic;
+        sdram_cke  : out    std_logic;
+        sdram_csn  : out    std_logic;
+        sdram_casn : out    std_logic;
+        sdram_rasn : out    std_logic;
+        sdram_wen  : out    std_logic;
+        sdram_dqm  : out    std_logic_vector(1 downto 0);
+        sdram_ba   : out    std_logic_vector(1 downto 0);
+        sdram_a    : out    std_logic_vector(12 downto 0);
+        sdram_d    : inout std_logic_vector(15 downto 0)
+    );
+end toplevel;
+
+architecture rtl of toplevel is
+    signal arst : std_logic;
+    signal axi_initiator : axi4l_initiator_signals_t;
+    signal axi_target    : axi4l_target_signals_t;
+    signal dq_o : std_logic_vector(15 downto 0);
+    signal dq_i : std_logic_vector(15 downto 0);
+    signal dq_oe : std_logic;
+    signal dqml : std_logic;
+    signal dqmh : std_logic;
+    signal clk : std_logic;
+begin
+
+    clk <= clk_25mhz;
+    sdram_d <= dq_o when dq_oe = '1' else (others => 'Z');
+    sdram_dqm <= dqml & dqmh;
+
+    basic_sdram_0: entity work.basic_sdram
+    generic map(
+        clk_period => 40 ns, -- TODO use a PLL for better speeds
+        required_power_on_wait => 200 us
+    )
+    port map (
+        clk           => clk,
+        arst          => arst,
+        axi_initiator => axi_initiator,
+        axi_target    => axi_target,
+        cke           => sdram_cke,
+        cs_l          => sdram_csn,
+        cas_l         => sdram_casn,
+        ras_l         => sdram_rasn,
+        we_l          => sdram_wen,
+        dqml          => dqml,
+        dqmh          => dqmh,
+        ba            => sdram_ba,
+        a             => sdram_a,
+        dq_o          => dq_o,
+        dq_i          => dq_i,
+        dq_oe         => dq_oe
+    );
+end architecture rtl;
